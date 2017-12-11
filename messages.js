55c319323ac002b81b86e9376dbcdfe77b68b233
Mukul <mukul1904@gmail.com>
2017-12-11 16:44:55 +0530

    Add mesages file

---
+++ messages.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    'titleMessage': 'Welcome to Kids Classic books',
+    'greetingMessage': 'Welcome to Kids Classic books',
+    'repromptMessage': 'Please repeat',
+    'goodByeMessgae': 'Good Bye',
+    'helpMessage': 'You can ask this skills, \'all-time most popular children books\', \'most popular children books of this week\''
+}
