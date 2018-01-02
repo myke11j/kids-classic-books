967949e3da5945995e5f875d7ab95bf2bcf4101c
Mukul <mukul1904@gmail.com>
2018-01-02 18:30:48 +0530

    Add new messages

--- messages.js
+++ messages.js
@@ -1,10 +1,16 @@
 'use strict';
 
 module.exports = {
-    'titleMessage': 'Welcome to Kids Classic books',
-    'greetingMessage': 'Welcome to Kids Classic books',
-    'repromptMessage': 'Please repeat',
-    'goodByeMessgae': 'Good Bye',
-    'notChildrenBook': ' is not a children book according to our data records.',
-    'helpMessage': 'You can ask this skills, \'all-time most popular children books\', \'most popular children books of this week\''
+    cardGreeting : () => 'Welcome to Kids Classic Books',
+    messageGreeting: () => 'Welcome to Kids Classic books. You can ask this skills, \'all-time most popular children books\', \'most popular children books of this week\' or book by title or author',
+    repromptGreeting: () => 'I\'m sorry, I am not able to hear your request. Please repeat or say \'help\' for sample requests',
+    cardHelp: () => 'Help from Kids Classic Books',
+    messageHelp: () => 'You can ask this skills, \'all-time most popular children books\', \'most popular children books of this week\' or book by title or author',
+    cardInvalidRequest: () => 'Kids Classic Books, unable to process request',
+    messageInvalidRequest: () => 'I\'m sorry. I was not able to retrieve book title or author from your request. A sample reuqest can be \'Tell me about Harry about from J.K. Rowlings\'',
+    cardIneligibleRequest: () => 'Kids Classic Books, non-children book requested',
+    messageIneligibleRequest: (book) => `${book} is not a children book according to our data records.`,
+    cardGoodBye: () => 'Good Bye from Kids Classic Books',
+    messageGoodBye: () => 'Good Bye',
+    messageReprompt: () => 'I\'m sorry, I am not able to hear your request. Please repeat or say \'help\' for sample requests',
 }
